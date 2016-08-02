8d88f432c86bb3213063b1e04512ffe4013f4019
Daniel Kawamoto <djkawamoto@gmail.com>
2016-08-02 15:02:07 -0700

    Changed file structure, added css and js files

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function ingredient() {
+
+  var ingredient = document.getElementById('ingredient').value;
+
+  if (ingredient == 'Powdered Sugar') {
+    alert("You selected 'Powdered Sugar'")
+  }
+}
+
+function quantity() {
+
+  var quantity = document.getElementById('quantity').value;
+
+  if (quantity == '10') {
+    alert("You entered 10(qty).")
+  }
+}
