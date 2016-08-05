3939955014d0d641d5093dc716099a5f77d4b97a
Daniel Kawamoto <djkawamoto@gmail.com>
2016-08-04 17:52:04 -0700

    completed basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-function ingredient() {
+function calcGrams() {
 
+  var mass = {
+    "Creme Fraiche": 240,
+    "Heavy Cream": 240,
+    "Milk": 240,
+    "Sugar": 200,
+    "Brown Sugar": 220,
+    "Powdered Sugar": 130,
+    "Honey": 300,
+    "Molasses": 280,
+    "KosherSalt": 220,
+    "CocoaPowder": 120
+  };
   var ingredient = document.getElementById('ingredient').value;
-
-  if (ingredient == 'Powdered Sugar') {
-    alert("You selected 'Powdered Sugar'")
+  var quantity = document.getElementById('quantity').value;
+  if (quantity == null || quantity == "") {
+    quantity = 0;
   }
+// ################### add error handling for something other than a number
+  var getGrams = function (ingredient) {
+    return mass[ingredient];
+  };
+  var grams = quantity * getGrams(ingredient);
+  document.getElementById('result').value = grams;
 }
 
-function quantity() {
+function selectIngredient() {
+  document.getElementById('ingredient-label').innerHTML = document.getElementById('ingredient').value;
+}
 
-  var quantity = document.getElementById('quantity').value;
-
-  if (quantity == '10') {
-    alert("You entered 10(qty).")
-  }
+function quantityInput() {
+  document.getElementById('quantity-label').innerHTML = document.getElementById('quantity').value;
 }
