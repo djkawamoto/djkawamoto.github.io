e57192a612b148c4846d84b7d8be7a349ad6e040
Daniel Kawamoto <djkawamoto@gmail.com>
2016-08-12 23:53:09 -0700

    house cleaning

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,28 @@
 function calcGrams() {
 
-  var mass = {
-    "Creme Fraiche": 240,
-    "Heavy Cream": 240,
-    "Milk": 240,
-    "Sugar": 200,
-    "Brown Sugar": 220,
-    "Powdered Sugar": 130,
-    "Honey": 300,
-    "Molasses": 280,
-    "Kosher Salt": 220,
-    "Cocoa Powder": 120
-  };
-  var ingredient = document.getElementById('ingredient').value;
-  var quantity = document.getElementById('quantity').value;
-  if (quantity == null || quantity == "") {
-    quantity = 0;
-  }
-// ################### add error handling for something other than a number
-  var getGrams = function (ingredient) {
-    return mass[ingredient];
-  };
-  var grams = quantity * getGrams(ingredient);
-  document.getElementById('result').value = grams;
+    var mass = {
+        "Creme Fraiche": 240,
+        "Heavy Cream": 240,
+        "Milk": 240,
+        "Sugar": 200,
+        "Brown Sugar": 220,
+        "Powdered Sugar": 130,
+        "Honey": 300,
+        "Molasses": 280,
+        "Kosher Salt": 220,
+        "Cocoa Powder": 120
+    };
+
+    var ingredient = document.getElementById('ingredient').value;
+    var quantity = document.getElementById('quantity').value;
+
+    var grams = quantity * mass[ingredient];
+    if (isNaN(grams)) {
+      grams = "OOPS!";
+    };
+    document.getElementById('result').value = grams;
 }
 
-function selectIngredient() {
-  document.getElementById('ingredient-label').innerHTML = document.getElementById('ingredient').value;
-}
-
-function quantityInput() {
-  document.getElementById('quantity-label').innerHTML = document.getElementById('quantity').value;
-}
+function submitIngredient() {
+  window.open="mailto:djkawamoto@gmail.com?subject=New%20Ingredient%20Submission&body=Ingredient%20Name:%20%0D%0AIngredient%20Mass%20in%20Grams:%20";
+;}
