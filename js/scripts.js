4e0f233c1ddf824ac7478931c19d98ea970050bd
Aaron Medina <admedina10@gmail.com>
2016-09-17 12:32:00 -0700

    Replace JavaScript script.js with jQuery toGrams.js
    I made the potential for adding items to this easier.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-function calcGrams() {
+// function calcGrams() {
 
-    var mass = {
-        "Creme Fraiche": 240,
-        "Heavy Cream": 240,
-        "Milk": 240,
-        "Sugar": 200,
-        "Brown Sugar": 220,
-        "Powdered Sugar": 130,
-        "Honey": 300,
-        "Molasses": 280,
-        "Kosher Salt": 220,
-        "Cocoa Powder": 120
-    };
+    // var mass = {
+        // "Creme Fraiche": 240,
+        // "Heavy Cream": 240,
+        // "Milk": 240,
+        // "Sugar": 200,
+        // "Brown Sugar": 220,
+        // "Powdered Sugar": 130,
+        // "Honey": 300,
+        // "Molasses": 280,
+        // "Kosher Salt": 220,
+        // "Cocoa Powder": 120
+    // };
 
-    var ingredient = document.getElementById('ingredient').value;
-    var quantity = document.getElementById('quantity').value;
+    // var ingredient = document.getElementById('ingredient').value;
+    // var quantity = document.getElementById('quantity').value;
 
-    var grams = quantity * mass[ingredient];
-    if (isNaN(grams)) {
-      grams = "OOPS!";
-    };
-    document.getElementById('result').value = grams;
-}
+    // var grams = quantity * mass[ingredient];
+    // if (isNaN(grams)) {
+      // grams = "OOPS!";
+    // };
+    // document.getElementById('result').value = grams;
+// }
